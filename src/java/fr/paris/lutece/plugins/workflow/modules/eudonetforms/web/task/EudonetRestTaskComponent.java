f02d2a7bc53e2c640120115b98afb67f15ebdc0c
Guillaume Lépine <guillaume.lepine@sogeti.com>
2020-09-09 12:25:04 +0200

    WFEUDOFO-2 : Adds config form with Rest service info

--- src/java/fr/paris/lutece/plugins/workflow/modules/eudonetforms/web/task/EudonetRestTaskComponent.java
+++ src/java/fr/paris/lutece/plugins/workflow/modules/eudonetforms/web/task/EudonetRestTaskComponent.java
@@ -1,15 +1,30 @@
 package fr.paris.lutece.plugins.workflow.modules.eudonetforms.web.task;
 
+import java.util.HashMap;
 import java.util.Locale;
+import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
+import fr.paris.lutece.plugins.forms.business.FormHome;
+import fr.paris.lutece.plugins.workflow.modules.eudonetforms.business.EudonetRestTaskConfig;
 import fr.paris.lutece.plugins.workflow.web.task.AbstractTaskComponent;
 import fr.paris.lutece.plugins.workflowcore.service.task.ITask;
+import fr.paris.lutece.portal.service.template.AppTemplateService;
+import fr.paris.lutece.util.html.HtmlTemplate;
 
 public class EudonetRestTaskComponent extends AbstractTaskComponent
 {
-
+    // Mark
+    private static final String MARK_FORM_LIST = "form_list";
+    private static final String MARK_CONFIG = "config";
+    
+    // Templates
+    private static final String TEMPLATE_TASK_EUDONET_REST_CONFIG = "admin/plugins/workflow/modules/eudonetforms/task_eudonetfoms_rest_config.html";
+    
+    
+    private EudonetRestTaskConfig _config;
+    
     @Override
     public String getDisplayTaskForm( int nIdResource, String strResourceType, HttpServletRequest request, Locale locale, ITask task )
     {
@@ -20,8 +35,18 @@ public class EudonetRestTaskComponent extends AbstractTaskComponent
     @Override
     public String getDisplayConfigForm( HttpServletRequest request, Locale locale, ITask task )
     {
-        // TODO Auto-generated method stub
-        return null;
+        _config = getTaskConfigService( ).findByPrimaryKey( task.getId( ) );
+        if ( _config == null )
+        {
+            _config = new EudonetRestTaskConfig( );
+        }
+        
+        Map<String, Object> model = new HashMap<>( );
+        model.put( MARK_FORM_LIST, FormHome.getFormsReferenceList( ) );
+        model.put( MARK_CONFIG, _config );
+        
+        HtmlTemplate template = AppTemplateService.getTemplate( TEMPLATE_TASK_EUDONET_REST_CONFIG, locale, model );
+        return template.getHtml( );
     }
 
     @Override
